549944268dfb0d9a33938c23a3019daef0ca4f17
AdamH03 <118226971+AdamH03@users.noreply.github.com>
2023-12-01 21:00:57 +0000

    Updated App.java
    Added Javadoc Comments

--- src/App.java
+++ src/App.java
@@ -1,7 +1,15 @@
+/**
+ * This class starts the application.
+ */
 public class App {
+    /**
+     * The entry point of the application.
+     *
+     * @param args Command-line arguments.
+     */
     public static void main(String[] args){
         CommandLineInterface app = new CommandLineInterface();
-        
         app.run();
     }
 }
+
